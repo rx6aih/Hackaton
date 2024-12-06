76c1ddb550b9a5df1d8bd2b9c4590e14fee9532e
rx6aih <mrsaintsteam@mail.ru>
2024-12-07 01:32:18 +0300

    Закончил почти всю вёрстку

--- src/Components/UI/Text/TextBox.jsx
+++ src/Components/UI/Text/TextBox.jsx
@@ -4,7 +4,7 @@ const TextBox = ({title, placeholder}) => {
     return (
         <div className={"flex flex-col w-full"}>
             <p className={"font-medium text-xl mb-2"}>{title}</p>
-            <input className={"w-full rounded-lg p-1 border-gray-300 border-2 hover:bg-gray-50 "} placeholder={placeholder} type={"text"}/>
+            <input className={"w-full rounded-lg p-2 border-gray-300 border-2 hover:bg-gray-50 "} placeholder={placeholder} type={"text"}/>
         </div>
     );
 };
